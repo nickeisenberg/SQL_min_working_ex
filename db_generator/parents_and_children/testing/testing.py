ebfc3f9ca13afc5d8fca0f6366cd79295d362704
Nick Eisenberg <nickeisenberg@gmail.com>
2023-08-21 11:14:17 -0700

    engine generator function

--- db_generator/parents_and_children/testing/testing.py
+++ db_generator/parents_and_children/testing/testing.py
@@ -3,10 +3,18 @@ from sqlalchemy.orm import declarative_base as Base
 from parents_and_children.create import Create 
 import numpy as np
 import pandas as pd
+from utils import engine_generator
 
 # mysql
-engine = db.create_engine(
-    'mysql+pymysql://root:@127.0.0.1:3306/practice?unix_socket=/tmp/mysql.sock'
+
+engine = engine_generator(
+    dialect='mysql', 
+    driver='pymysql', 
+    username='root', 
+    host='127.0.0.1',
+    port='3306',
+    db='practice',
+    unix_socket='/tmp/mysql.sock'
 )
 
 # sqlite
@@ -15,14 +23,15 @@ engine = db.create_engine(
 # engine = db.create_engine(f'sqlite:///{path}')
 
 # initialize the class and the database
-
 base = Base()
 database = Create(engine=engine, base=base)
 
-database.initialize(no_parents=10, no_children=10)
+database.initialize(no_parents=10, no_children=5)
 
 # add some dependents after the fact
 emp_df = pd.read_sql("""select * from employment""", engine)
+
+emp_df.shape
 
 emp_df.columns.values
 
